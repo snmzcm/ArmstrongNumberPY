f717f76c873bcf47586a1734d2ceeb0b1630c3bb
snmzcm <snmzcm@users.noreply.github.com>
2022-06-29 20:33:11 +0300

    Add files via upload

---
+++ armstrng.py
@@ -0,0 +1,24 @@
+#Armstrong Check script
+#Written by https://github.com/snmzcm
+def main():
+    numberz = input("Enter a number for Armstrong Number Check: ")
+    numberCheck = int(numberz)
+    digits = len(numberz)
+    totalsum = 0
+    #print(digits)
+    for i in numberz:
+        i = int(i)
+        sum = i ** digits
+        totalsum += sum
+        print(f"Digit: {i} ^ {digits} equals to {sum}")
+    if totalsum == numberCheck:
+        print(f"{numberCheck} is an Armstrong Number ")
+    else:
+        printf("{numberCheck} is not an Armstrong Number")
+
+
+
+
+
+
+main()
